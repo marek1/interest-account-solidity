534be10142ba3df20fdb7fe5cad643b1b0a4caf9
marek sonnabend <marek.sonnabend@ext.flaschenpost.de>
2021-12-12 11:25:51 +0100

    Calculate interest + days accrued

--- contracts/InterestAccount.sol
+++ contracts/InterestAccount.sol
@@ -2,7 +2,10 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract InterestAccount {
-    uint duration = 0;
+    uint interest = 5;
+    uint minDuration = 90;
+    uint daysInMilliseconds = 86400000;
+    uint daysInOneYear = 365;
     mapping (address => uint) balances;
     mapping (address => uint) depositDay;
 
@@ -10,20 +13,23 @@ contract InterestAccount {
         require(balances[depositAddress] == 0, "Only 1 deposit per address");
         require(amount > 0, "Deposit an amount > 0 eth");
         balances[depositAddress] += amount;
-        depositDay[depositAddress] = block.timestamp; // - 91 days;
+        depositDay[depositAddress] = block.timestamp;
     }
 
-    function withdraw(address depositAddress, uint amount) public {
-        require(amount <= balances[depositAddress], "Withdraw amount exceeds Deposit");
-        require(canWithdraw(depositAddress), "Withdraw only possible after 90 days");
-        balances[depositAddress] -= amount;
+    function withdraw(address depositAddress) public {
+        require(getDaysAccrued(depositAddress) > minDuration, "Withdraw only possible after minimum days");
+        balances[depositAddress] -= getBalance(depositAddress) + getInterestAccrued(depositAddress);
     }
 
     function getBalance(address depositAddress) public view returns(uint) {
         return balances[depositAddress];
     }
 
-    function canWithdraw(address depositAddress) public view returns(bool) {
-        return block.timestamp >= depositDay[depositAddress] + (duration * 1 days);
+    function getDaysAccrued(address depositAddress) private view returns(uint) {
+        return (block.timestamp - depositDay[depositAddress]) / daysInMilliseconds;
+    }
+
+    function getInterestAccrued(address depositAddress) private view returns(uint) {
+        return (getDaysAccrued(depositAddress) / daysInOneYear) * interest;
     }
 }
